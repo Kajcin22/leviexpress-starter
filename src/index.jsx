3f9c7a21da23110495c91ef9351c4f034c10627c
Kajcin22 <masekkaterina@gmail.com>
2022-05-21 10:33:32 +0200

    přidán react router

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+
 import { App } from './components/App';
+import { Home } from './components/Home';
+import Reservation from './components/Reservation';
 import './style.css';
 
 export const API_BASE_URL = 'http://leviexpress-backend.herokuapp.com/api';
 
-createRoot(document.querySelector('#app')).render(<App />, );
+createRoot(document.querySelector('#app')).render(<App />);
